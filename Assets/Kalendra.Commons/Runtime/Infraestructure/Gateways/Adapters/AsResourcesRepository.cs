24ad443c4a21e04d8edba536a2b04b514c762f06
RGV <ricardo.guzman.velasco@outlook.com>
2020-12-07 19:23:39 +0100

    ReadOnlyRepository using Resources.LoadAsync
    Scene Test with DummyLoader logging results.

---
+++ Assets/Kalendra.Commons/Runtime/Infraestructure/Gateways/Adapters/AsResourcesRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kalendra.Commons.Runtime.Domain.Gateways;
+using UnityEngine;
+
+namespace Kalendra.Commons.Utils.StaticExtensions.Adapters
+{
+    public class ByResourcesNameNotAsyncRepository<T> : IReadOnlyRepository<T> where T : Object
+    {
+        public async Task<IEnumerable<T>> LoadAll() 
+        {
+            var loadedCollection = Resources.LoadAll<T>("");
+            return loadedCollection;
+        }
+
+        public async Task<T> Load(string hashID)
+        {
+            var loaded = Resources.Load<T>(hashID);
+            return loaded;
+        }
+    }
+}
