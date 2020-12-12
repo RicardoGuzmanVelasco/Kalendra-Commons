f1bed10ce923fb501ab47b1582263847a8a19737
RGV <ricardo.guzman.velasco@outlook.com>
2020-12-12 21:34:24 +0100

    Acceptance async test to CharacterClass

--- Assets/Kalendra.Commons/Tests/Runtime/Acceptance/CharacterClassesAcceptanceTests.cs
+++ Assets/Kalendra.Commons/Tests/Runtime/Acceptance/CharacterClassesAcceptanceTests.cs
@@ -1,17 +1,19 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
 using Kalendra.Commons.Runtime.Domain.Gateways;
 using Kalendra.Commons.Runtime.Infraestructure.CharacterTaxonomySystem.ScriptableObjects;
 using Kalendra.Commons.Runtime.Infraestructure.Gateways.Adapters;
 using NUnit.Framework;
 
-namespace Kalendra.Commons.Tests.Runtime.Infraestructure
+namespace Kalendra.Commons.Tests.Runtime.Acceptance
 {
+    [TestFixture, Category("Acceptance")]
     public class CharacterClassesAcceptanceTests
     {
         readonly IReadOnlyRepository<CharacterClassDefinition> loader =
             new ByResourcesNameNotAsyncRepository<CharacterClassDefinition>();
 
-        [Test, Category("Acceptance")]
+        [Test]
         public async void NoCharacterClassDefinition_HasVoidID()
         {
             var definitions = await loader.LoadAll();
@@ -19,6 +21,17 @@ namespace Kalendra.Commons.Tests.Runtime.Infraestructure
             foreach(var definition in definitions)
                 string.IsNullOrWhiteSpace(definition.ToDefined().ID).Should()
                     .BeFalse($"{definition.name} musn't have void ID");
+        }
+        
+        [Test]
+        public async void NoCharacterClassDefinition_HasDuplicatedID()
+        {
+            var definitions = await loader.LoadAll();
+
+            foreach(var definition in definitions)
+                definitions
+                    .Count(classDefinition => classDefinition.ToDefined().ID == definition.ToDefined().ID)
+                    .Should().Be(1, $"{definition.name} shouldn't be repeated in other definition");
         }
     }
 }
