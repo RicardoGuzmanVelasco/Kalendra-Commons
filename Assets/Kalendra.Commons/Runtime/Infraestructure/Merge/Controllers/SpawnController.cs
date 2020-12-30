ef3ae06553362402907cfb2eaa484e073dbd27ce
RGV <ricardo.guzman.velasco@outlook.com>
2020-12-30 10:30:58 +0100

    SpawnController (dummy)

---
+++ Assets/Kalendra.Commons/Runtime/Infraestructure/Merge/Controllers/SpawnController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Kalendra.Commons.Runtime.Domain.BoardSystem.UseCases;
+using UnityEngine;
+
+namespace Kalendra.Commons.Runtime.Infraestructure.Merge.Controllers
+{
+    public class SpawnController : MonoBehaviour
+    {
+        ISpawnInputReceiver spawnReceiver;
+
+        void Update()
+        {
+            if(Input.anyKeyDown)
+                spawnReceiver.Request();
+        }
+    }
+}
