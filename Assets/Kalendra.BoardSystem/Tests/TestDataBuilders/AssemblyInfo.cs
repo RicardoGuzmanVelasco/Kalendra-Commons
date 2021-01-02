1c7e20f7e88ddd5f4846a6ab71aadf687ea930a2
RGV <ricardo.guzman.velasco@outlook.com>
2021-01-02 10:44:47 +0100

    BoardSystem asmdefs

---
+++ Assets/Kalendra.BoardSystem/Tests/TestDataBuilders/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("Kalendra")]
+[assembly: AssemblyProduct("BoardSystem")]
+[assembly: AssemblyTitle("Editor.Tests.Builders")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyCopyright("(C) 2020 RGV")]
+[assembly: InternalsVisibleTo("Kalendra.BoardSystem.Tests")]
+[assembly: InternalsVisibleTo("Kalendra.BoardSystem.Editor.Tests")]
