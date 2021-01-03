dac50fb83271a52829b6be6237d585c649d41002
RGV <ricardo.guzman.velasco@outlook.com>
2021-01-03 10:20:16 +0100

    BoardSystem migration completed

---
+++ Assets/Kalendra.BoardSystem/Tests/TestDataBuilders/Domain/BoardBuilder.cs
@@ -0,0 +1,35 @@
+﻿using Kalendra.Commons.Runtime.Domain.BoardSystem;
+using Kalendra.Commons.Tests.TestDataBuilders.Builders;
+
+namespace Kalendra.Commons.Tests.TestDataBuilders.Domain.BoardSystem
+{
+    internal class BoardBuilder : Builder<Board>
+    {
+        int sizeX = 1, sizeY = 1;
+
+        #region Fluent API
+        public BoardBuilder WithSize(int sizeX, int sizeY)
+        {
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+            
+            return this;
+        }
+        #endregion
+
+        #region ObjectMother/FactoryMethods
+        public static BoardBuilder New() => new BoardBuilder();
+
+        public static Board BoardWithNoTiles()
+        {
+            var boardObjectMother = new BoardBuilder().Build();
+            boardObjectMother.RemoveTile(0, 0);
+            return boardObjectMother;
+        }
+        #endregion
+
+        #region Builder implementation
+        public override Board Build() => new Board(sizeX, sizeY);
+        #endregion
+    }
+}
---
+++ Assets/Kalendra.BoardSystem/Tests/TestDataBuilders/Domain/TileContentMockBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Kalendra.Commons.Runtime.Domain.BoardSystem;
+using Kalendra.Commons.Tests.TestDataBuilders.Builders;
+
+namespace Kalendra.Commons.Tests.TestDataBuilders.Domain.BoardSystem
+{
+    internal class TileContentMockBuilder : MockBuilder<ITileContent>
+    {
+        TileContentMockBuilder() { }
+        
+        #region ObjectMother/FactoryMethods
+        public static TileContentMockBuilder New() => new TileContentMockBuilder();
+        #endregion
+    }
+}
