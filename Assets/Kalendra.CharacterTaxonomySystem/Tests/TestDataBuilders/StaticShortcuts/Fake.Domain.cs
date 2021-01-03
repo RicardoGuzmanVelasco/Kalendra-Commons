6d1c88d5c3f2aa0b52915d4fe412bf6b5261be53
RGV <ricardo.guzman.velasco@outlook.com>
2021-01-03 09:58:58 +0100

    CharacterTaxonomySystem builders

---
+++ Assets/Kalendra.CharacterTaxonomySystem/Tests/TestDataBuilders/StaticShortcuts/Fake.Domain.cs
@@ -0,0 +1,16 @@
+﻿using Kalendra.CharacterTaxonomySystem.Runtime.Domain.Entities;
+using NSubstitute;
+
+namespace Kalendra.CharacterTaxonomySystem.Tests.TestDataBuilders.StaticShortcuts
+{
+    internal static partial class Fake
+    {
+        internal static IClassDependantUsable ClassDependantUsable_UsableByClass(params CharacterClass[] classesToReturn)
+        {
+            var mock = Substitute.For<IClassDependantUsable>();
+            mock.AllowedClasses.ReturnsForAnyArgs(classesToReturn);
+
+            return mock;
+        }
+    }
+}
