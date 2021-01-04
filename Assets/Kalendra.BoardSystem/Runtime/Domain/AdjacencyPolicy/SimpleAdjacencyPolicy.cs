345c8aecd58c373f4842dbf3de4cd17a259c5747
RGV <ricardo.guzman.velasco@outlook.com>
2021-01-04 12:42:22 +0100

    SimpleAdjacencyPolicy tests

---
+++ Assets/Kalendra.BoardSystem/Runtime/Domain/AdjacencyPolicy/SimpleAdjacencyPolicy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Kalendra.BoardSystem.Runtime.Domain.Entities;
+
+namespace Kalendra.BoardSystem.Runtime.Domain.AdjacencyPolicy
+{
+    public abstract class SimpleAdjacencyPolicy : IAdjacencyPolicy
+    {
+        public IEnumerable<ITile> GetAdjacentTiles(IBoard board, ITile tile)
+        {
+            if(tile is null)
+                yield break;
+
+            var (x, y) = GetAdjacentCoords(tile.Coords);
+            
+            if(!board.HasTile(x, y))
+                yield break;
+            
+            yield return board[x, y];
+        }
+
+        protected abstract (int x, int y) GetAdjacentCoords((int x, int y) coords);
+    }
+    
+    public class UpAdjacencyPolicy : SimpleAdjacencyPolicy
+    {
+        protected override (int x, int y) GetAdjacentCoords((int x, int y) coords) => (coords.x, coords.y + 1);
+    }
+
+    public class UpRightAdjacencyPolicy : SimpleAdjacencyPolicy
+    {
+        protected override (int x, int y) GetAdjacentCoords((int x, int y) coords) => (coords.x + 1, coords.y + 1);
+    }
+    
+    public class RightAdjacencyPolicy : SimpleAdjacencyPolicy
+    {
+        protected override (int x, int y) GetAdjacentCoords((int x, int y) coords) => (coords.x + 1, coords.y);
+    }
+    
+    public class RightDownAdjacencyPolicy : SimpleAdjacencyPolicy
+    {
+        protected override (int x, int y) GetAdjacentCoords((int x, int y) coords) => (coords.x + 1, coords.y - 1);
+    }
+    
+    public class DownAdjacencyPolicy : SimpleAdjacencyPolicy
+    {
+        protected override (int x, int y) GetAdjacentCoords((int x, int y) coords) => (coords.x, coords.y - 1);
+    }
+
+    public class LeftDownAdjacencyPolicy : SimpleAdjacencyPolicy
+    {
+        protected override (int x, int y) GetAdjacentCoords((int x, int y) coords) => (coords.x - 1, coords.y - 1);
+    }
+    
+    public class LeftAdjacencyPolicy : SimpleAdjacencyPolicy
+    {
+        protected override (int x, int y) GetAdjacentCoords((int x, int y) coords) => (coords.x - 1, coords.y);
+    }
+    
+    public class UpLeftAdjacencyPolicy : SimpleAdjacencyPolicy
+    {
+        protected override (int x, int y) GetAdjacentCoords((int x, int y) coords) => (coords.x - 1, coords.y + 1);
+    }
+}
---
+++ Assets/Kalendra.BoardSystem/Tests/Editor/Domain/SimpleAdjacencyTests.cs
@@ -0,0 +1,291 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Kalendra.BoardSystem.Runtime.Domain.AdjacencyPolicy;
+using Kalendra.BoardSystem.Tests.TestDataBuilders.StaticShortcuts;
+using NUnit.Framework;
+
+namespace Kalendra.BoardSystem.Tests.Editor
+{
+    public class SimpleAdjacencyTests
+    {
+        #region Up
+        [Test]
+        public void Up_Null_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            UpAdjacencyPolicy sut = new UpAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, null);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Up_OutOfRange_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            UpAdjacencyPolicy sut = new UpAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Up_InRange_ReturnsTileAbove()
+        {
+            var board = Build.Board().WithSize(1, 2).Build();
+            UpAdjacencyPolicy sut = new UpAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Single().Coords.Should().Be((0, 1));
+        }
+        #endregion
+        
+        #region Down
+        [Test]
+        public void Down_Null_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            DownAdjacencyPolicy sut = new DownAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, null);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Down_OutOfRange_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            DownAdjacencyPolicy sut = new DownAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Down_InRange_ReturnsTileBelow()
+        {
+            var board = Build.Board().WithSize(1, 2).Build();
+            DownAdjacencyPolicy sut = new DownAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 1]);
+
+            result.Single().Coords.Should().Be((0, 0));
+        }
+        #endregion
+        
+        #region Right
+        [Test]
+        public void RightAdjacency_NullTile_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            RightAdjacencyPolicy sut = new RightAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, null);
+
+            result.Should().BeEmpty();
+        }
+        
+        [Test]
+        public void RightAdjacency_OutOfRange_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            RightAdjacencyPolicy sut = new RightAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void RightAdjacency_InRange_ReturnsTileLeftwards()
+        {
+            var board = Build.Board().WithSize(2, 1).Build();
+            RightAdjacencyPolicy sut = new RightAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Single().Coords.Should().Be((1, 0));
+        }
+        #endregion
+        
+        #region Left
+        [Test]
+        public void LeftAdjacency_NullTile_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            LeftAdjacencyPolicy sut = new LeftAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, null);
+
+            result.Should().BeEmpty();
+        }
+        
+        [Test]
+        public void LeftAdjacency_OutOfRange_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            LeftAdjacencyPolicy sut = new LeftAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void LeftAdjacency_InRange_ReturnsTileRightwards()
+        {
+            var board = Build.Board().WithSize(2, 1).Build();
+            LeftAdjacencyPolicy sut = new LeftAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[1, 0]);
+
+            result.Single().Coords.Should().Be((0, 0));
+        }
+        #endregion
+        
+        #region UpRight
+        [Test]
+        public void UpRight_Null_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            UpRightAdjacencyPolicy sut = new UpRightAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, null);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void UpRight_OutOfRange_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            UpRightAdjacencyPolicy sut = new UpRightAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void UpRight_InRange_ReturnsTileAbove()
+        {
+            var board = Build.Board().WithSize(2, 2).Build();
+            UpRightAdjacencyPolicy sut = new UpRightAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Single().Coords.Should().Be((1, 1));
+        }
+        #endregion
+        
+        #region RightDown
+        [Test]
+        public void RightDown_Null_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            RightDownAdjacencyPolicy sut = new RightDownAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, null);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void RightDown_OutOfRange_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            RightDownAdjacencyPolicy sut = new RightDownAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void RightDown_InRange_ReturnsTileAbove()
+        {
+            var board = Build.Board().WithSize(2, 2).Build();
+            RightDownAdjacencyPolicy sut = new RightDownAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 1]);
+
+            result.Single().Coords.Should().Be((1, 0));
+        }
+        #endregion
+        
+        #region LeftDown
+        [Test]
+        public void LeftDown_Null_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            LeftDownAdjacencyPolicy sut = new LeftDownAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, null);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void LeftDown_OutOfRange_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            LeftDownAdjacencyPolicy sut = new LeftDownAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void LeftDown_InRange_ReturnsTileAbove()
+        {
+            var board = Build.Board().WithSize(2, 2).Build();
+            LeftDownAdjacencyPolicy sut = new LeftDownAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[1, 1]);
+
+            result.Single().Coords.Should().Be((0, 0));
+        }
+        #endregion
+
+        #region UpLeft
+        [Test]
+        public void UpLeft_Null_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            UpLeftAdjacencyPolicy sut = new UpLeftAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, null);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void UpLeft_OutOfRange_ReturnsEmpty()
+        {
+            var board = Build.Board().Build();
+            UpLeftAdjacencyPolicy sut = new UpLeftAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[0, 0]);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void UpLeft_InRange_ReturnsTileAbove()
+        {
+            var board = Build.Board().WithSize(2, 2).Build();
+            UpLeftAdjacencyPolicy sut = new UpLeftAdjacencyPolicy();
+
+            var result = sut.GetAdjacentTiles(board, board[1, 0]);
+
+            result.Single().Coords.Should().Be((0, 1));
+        }
+        #endregion
+    }
+}
