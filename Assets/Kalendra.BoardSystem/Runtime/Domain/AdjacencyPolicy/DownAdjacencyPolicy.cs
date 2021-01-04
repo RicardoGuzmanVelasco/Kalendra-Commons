50165d6de10a7ccf430c32a825529a624c8a239f
RGV <ricardo.guzman.velasco@outlook.com>
2021-01-04 12:06:10 +0100

    Up&Down AdjacencyPolicy

---
+++ Assets/Kalendra.BoardSystem/Runtime/Domain/AdjacencyPolicy/DownAdjacencyPolicy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Kalendra.BoardSystem.Runtime.Domain.Entities;
+
+namespace Kalendra.BoardSystem.Runtime.Domain.AdjacencyPolicy
+{
+    public class DownAdjacencyPolicy : IAdjacencyPolicy
+    {
+        public IEnumerable<ITile> GetAdjacentTiles(IBoard board, ITile tile)
+        {
+            if(tile is null)
+                yield break;
+
+            var (x, y) = GetAdjacentCoords(tile.Coords);
+            
+            if(!board.HasTile(x, y))
+                yield break;
+            
+            yield return board.GetTile(x, y);
+        }
+
+        (int x, int y) GetAdjacentCoords((int x, int y) coords)
+        {
+            return (coords.x, coords.y - 1);
+        }
+    }
+}
---
+++ Assets/Kalendra.BoardSystem/Runtime/Domain/AdjacencyPolicy/IAdjacencyPolicy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Kalendra.BoardSystem.Runtime.Domain.Entities;
+
+namespace Kalendra.BoardSystem.Runtime.Domain.AdjacencyPolicy
+{
+    public interface IAdjacencyPolicy
+    {
+        IEnumerable<ITile> GetAdjacentTiles(IBoard board, ITile tile);
+    }
+}
