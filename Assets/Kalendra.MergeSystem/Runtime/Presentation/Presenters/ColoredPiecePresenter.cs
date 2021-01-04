9c38efc1307a57b7cf22078c159b27386b04dab8
RGV <ricardo.guzman.velasco@outlook.com>
2021-01-04 14:08:46 +0100

    Packages updating

---
+++ Assets/Kalendra.MergeSystem/Runtime/Presentation/Presenters/ColoredPiecePresenter.cs
@@ -0,0 +1,23 @@
+﻿using Kalendra.Commons.Runtime.Architecture.Boundaries;
+using UnityEngine;
+
+public class ColoredPiecePresenter : MonoBehaviour, IBoundaryOutputPort<ColoredPieceDTO>
+{
+    public void Response(ColoredPieceDTO pieceToDraw)
+    {
+        //TODO: data from DTO to view 
+        Debug.Log(pieceToDraw);
+    }
+}
+
+public class ColoredPieceDTO : IBoundaryResponseDTO
+{
+    public int tier;
+    public string pieceID;
+    public string colorID;
+
+    public override string ToString()
+    {
+        return $"{nameof(tier)}: {tier}, {nameof(pieceID)}: {pieceID}, {nameof(colorID)}: {colorID}";
+    }
+}
