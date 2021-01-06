aa97ee3460a2cbff542feace203886d4c5ab172e
RGV <ricardo.guzman.velasco@outlook.com>
2021-01-06 11:50:32 +0100

    Row/Column BoardQueries result

--- Assets/Kalendra.BoardSystem/Runtime/Domain/BoardQueries/IBoardQuery.cs
+++ Assets/Kalendra.BoardSystem/Runtime/Domain/BoardQueries/IBoardQuery.cs
@@ -1,23 +1,9 @@
-﻿using System.Collections.Generic;
-using Kalendra.BoardSystem.Runtime.Domain.Entities;
+﻿using Kalendra.BoardSystem.Runtime.Domain.Entities;
 
 namespace Kalendra.BoardSystem.Runtime.Domain.BoardQueries
 {
-    
     public interface IBoardQuery<out T> where T : IBoardQueryResult 
     {
         T Request(IBoard board);
-    }
-    
-    public interface IBoardQueryResult{ }
-
-    public class TilesBoardQueryResult : IBoardQueryResult
-    {
-        public readonly IEnumerable<ITile> tiles;
-
-        public TilesBoardQueryResult(IEnumerable<ITile> tiles)
-        {
-            this.tiles = tiles;
-        }
     }
 }
---
+++ Assets/Kalendra.BoardSystem/Runtime/Domain/BoardQueries/IBoardQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Kalendra.BoardSystem.Runtime.Domain.Entities;
+
+namespace Kalendra.BoardSystem.Runtime.Domain.BoardQueries
+{
+    public interface IBoardQueryResult { }
+
+    public class TilesBoardQueryResult : IBoardQueryResult
+    {
+        public readonly IEnumerable<ITile> tiles;
+
+        public TilesBoardQueryResult(IEnumerable<ITile> tiles)
+        {
+            this.tiles = tiles;
+        }
+    }
+}
